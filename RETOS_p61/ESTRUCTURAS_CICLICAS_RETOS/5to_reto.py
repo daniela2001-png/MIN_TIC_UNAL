17d81e4042364f2ae6513ca61d2d44b1aaa02195
daniela2001-png <dmbmora@outlook.com>
2021-06-02 21:06:12 -0500

    añado punto 5

--- RETOS_p61/ESTRUCTURAS_CICLICAS_RETOS/5to_reto.py
+++ RETOS_p61/ESTRUCTURAS_CICLICAS_RETOS/5to_reto.py
@@ -2,4 +2,11 @@
 """
 Calcular el valor de 2 elevado a la potencia n
 """
-def potencia():
+
+
+def potencia(n):
+    result = [2 ** i for i in range(n + 1)]
+    return result
+
+
+print(potencia(2))
